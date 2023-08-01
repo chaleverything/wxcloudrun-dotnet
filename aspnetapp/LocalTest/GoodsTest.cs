2774634a4044c9e4d8fcc3b17e02e65e088c74c9
chaleverything <344673824@qq.com>
2023-08-01 17:00:43 +0800

    tj

--- aspnetapp/LocalTest/GoodsTest.cs
+++ aspnetapp/LocalTest/GoodsTest.cs
@@ -1,17 +1,6 @@
-﻿using aspnetapp.Controllers;
-using Common.Enumeraties;
-using Common.Utilities;
-using Models;
-
-namespace aspnetapp.LocalTest
+﻿namespace aspnetapp.LocalTest
 {
     public static class GoodsTest
     {
-        public static async Task GetGoodsInfoTestAsync()
-        {
-            await new GoodsController().GetGoodsInfo(new GoodsSearch { pageIndex = 1, pageSize = 10 });
-        }
-
-
     }
 }
